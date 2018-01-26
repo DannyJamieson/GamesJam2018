8eafe831b9d02d69b25dab1fb3a444c9a24f6561
Ochamberlai2 <S179650@ucs.ac.uk>
2018-01-26 22:40:40 +0000

    Lever half working

---
+++ Unity/Project/Assets/OliverScripts/LeverOpenDoorway.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverOpenDoorway : MonoBehaviour {
+
+    public bool blackActive;
+    public bool whiteActive;
+
+    private GameObject door;
+
+
+    private void Awake()
+    {
+        door = transform.Find("Doorway").Find("Door").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if(blackActive && whiteActive)
+        {
+            door.SetActive(false);
+        }
+	}
+}
